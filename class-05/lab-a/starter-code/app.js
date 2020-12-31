363a59d0649bcf19e0e8b67847c3e40538dde790
Qsalman <qosaisalman@gmail.com>
2020-12-31 15:44:33 +0200

    Problem 3 sloved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -79,11 +79,25 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    var sumAndMultiplyArray = []
 
+    var aPlusB = sum(a, b)[0];
+    var finalSum = sum(aPlusB, c)[0];
+    sumAndMultiplyArray[0] = finalSum;
+  
+    var aTimesB = multiply(a, b)[0];
+    var finalProduct = multiply(aTimesB, c)[0];
+    sumAndMultiplyArray[1] = finalProduct;
+  
+    sumAndMultiplyArray[2] = `${a} and ${b} and ${c} sum to ${finalSum}.`
+    sumAndMultiplyArray[3] = `The product of ${a} and ${b} and ${c} is ${finalProduct}.`
+  
+    return sumAndMultiplyArray;
 }
 
+
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
